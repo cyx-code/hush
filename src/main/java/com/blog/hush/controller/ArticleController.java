0af881b312ba6ed2e7dc4a10ad05fb8d72108490
cyx <>
2020-05-11 23:38:21 +0800

    更换图片位置，添加markdown文本编辑

--- src/main/java/com/blog/hush/controller/ArticleController.java
+++ src/main/java/com/blog/hush/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.blog.hush.controller;
 
 import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/article")
 @Api(value = "ArticleController", tags = {"文章管理接口"})
 public class ArticleController {
-
 }
--- src/main/java/com/blog/hush/controller/admin/AdminController.java
+++ src/main/java/com/blog/hush/controller/admin/AdminController.java
@@ -2,6 +2,7 @@ package com.blog.hush.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
@@ -9,5 +10,10 @@ public class AdminController {
     @GetMapping("/admin")
     public String home() {
         return "admin/index";
+    }
+
+    @GetMapping("/admin/article/write")
+    public String writeArticle() {
+        return "admin/article/write";
     }
 }

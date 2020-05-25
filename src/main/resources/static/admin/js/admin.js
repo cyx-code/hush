2645cedc8f0dcdf27b07fb5c6be74e7187eae140
cyx-code <1741914950@qq.com>
2020-05-25 23:22:15 +0800

    优化项目结构，优化写文章后的提示

--- src/main/resources/static/admin/js/admin.js
+++ src/main/resources/static/admin/js/admin.js
@@ -1,3 +1,20 @@
+const tags = xmSelect.render({
+    el: '#tags',
+    name: 'tags',
+    prop: {
+        name: 'name',
+        value: 'id',
+    },
+    data: []
+});
+
+axios.get('/api/tag/list').then(response => {
+    const res = response.data;
+    tags.update({
+        data: res.data,
+        autoRow: true,
+    })
+});
 layui.use('element', function(){
     var element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
 
@@ -7,3 +24,37 @@ layui.use('element', function(){
         layer.msg(elem.text());
     });
 });
+
+layui.use('upload', function() {
+    var $ = layui.jquery
+        , upload = layui.upload;
+
+    //普通图片上传
+    var uploadInst = upload.render({
+        elem: '#img'
+        , url: '/admin/article/upload' //改成您自己的上传接口
+        , before: function (obj) {
+            //预读本地文件示例，不支持ie8
+            obj.preview(function (index, file, result) {
+                $('#imgShow').attr('src', result); //图片链接（base64）
+            });
+        }
+        , done: function (res) {
+            //如果上传失败
+            if (res.code > 0) {
+                return layer.msg('上传失败');
+            }
+            //上传成功
+            $('#cover').val(res.path);
+        }
+        , error: function () {
+            //演示失败状态，并实现重传
+            var demoText = $('#demoText');
+            demoText.html('<span style="color: #FF5722;">上传失败</span> <a class="layui-btn layui-btn-xs demo-reload">重试</a>');
+            demoText.find('.demo-reload').on('click', function () {
+                uploadInst.upload();
+            });
+        }
+    });
+});
+

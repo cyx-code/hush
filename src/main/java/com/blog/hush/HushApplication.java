6c34fbcaa7f793a7528726c1b371e335f8aef2f9
cyx-code <1741914950@qq.com>
2020-07-23 23:23:34 +0800

    数据库密码加密

--- src/main/java/com/blog/hush/HushApplication.java
+++ src/main/java/com/blog/hush/HushApplication.java
@@ -1,10 +1,12 @@
 package com.blog.hush;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 @MapperScan("com.blog.hush.mapper")
 public class HushApplication {
 
